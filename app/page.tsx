1f1a30b99f738b8ab28c89145c418919232954e0
obedgyamfi <obedgyamfi53@gmail.com>
2025-11-09 01:28:39 +0000

    Removed unused MatrixRain import as it caused build error

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,10 @@ import Hero from "@/components/Hero";
 import BlogSection from "@/components/BlogSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import About from "@/components/About";
-import MatrixRain from "@/components/MatrixRain";
 
 export default function Home() {
   return (
     <>
-      {/* <MatrixRain /> */}
-
       <Hero />
       <About />
       <ProjectsSection />
